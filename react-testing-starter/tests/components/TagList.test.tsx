b19b957d56e6a0047ea2c4b063b4f64c4fab8d4f
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-08-28 00:16:00 -0400

    🧠 test: Write test suite for asynchronous Taglist component using waitFor() and findAllByRole

--- react-testing-starter/tests/components/TagList.test.tsx
+++ react-testing-starter/tests/components/TagList.test.tsx
@@ -1,13 +1,23 @@
-import { render, screen, waitFor } from "@testing-library/react";
+// 1.) Type `itr` for import ceremony
+import { render, screen } from "@testing-library/react";
 import TagList from "../../src/components/TagList";
 
+// 2.) Create test for Taglist component
 describe("TagList", () => {
+  // 3.) Write a test case for the component redering tags correctly
   it("should render all tags from the list", async () => {
     render(<TagList />);
 
-    waitFor(() => {
-      const listItem = screen.getAllByRole("listitem");
-      expect(listItem.length).toBeGreaterThan(0);
-    });
+    // 6.) Use utility function in react testin library called waitFor(). This will continue to keep calling the callback you give it until it times out. The default time is 1000ms. So every 50ms the call back gets called by default. So after one second, if the things we looking for aren't found then our test will fail. Because of this you shouldn't have anything in the code that will cause side effects, otherwise the code that causes side effects will keep getting executed and you get unexpected results.
+    // await waitFor(() => {
+    //   // 4.) Declare [listItem] using getAllByRole
+    //   const listItems = screen.getAllByRole("listitem");
+    //   // 5.) Verify that the listItems has more than one item in it.
+    //   expect(listItems.length).toBeGreaterThan(0);
+    // });
+
+    // 7.) Instead of the previous implementation. Use findByAllRole which is a combination of waitFor and get query
+    const listItems = await screen.findAllByRole("listitem");
+    expect(listItems.length).toBeGreaterThan(0);
   });
 });
