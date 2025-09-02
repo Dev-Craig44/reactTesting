5af42160d841517b3590bda4e6ae38b19c2db1f4
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-09-01 23:38:13 -0400

    🧠 Setup mocks to handle finding by id

--- react-testing-starter/tests/mocks/handlers.ts
+++ react-testing-starter/tests/mocks/handlers.ts
@@ -1,5 +1,6 @@
 import { http, HttpResponse } from "msw";
 import ProductDetail from "../../src/components/ProductDetail";
+import { products } from "./data";
 import { db } from "./db";
 
 export const handlers = [
@@ -8,10 +9,17 @@ export const handlers = [
 ];
 ProductDetail;
 
+// 3.) Place this data into the newly created data.ts file and reference it in the {json} method;
 http.get("/products", () => {
-  return HttpResponse.json([
-    { id: 1, name: "Product 1" },
-    { id: 2, name: "Product 2" },
-    { id: 3, name: "Product 3" },
-  ]);
+  return HttpResponse.json(products);
+});
+
+// 1.) Define a new request handler
+http.get("/products/:id", ({ params }) => {
+  // 2.) Destructure this into it's [id] variable
+  const { id } = params;
+
+  // 5.) Write the logic to look up our product
+  // 6.) Because the [id] variable is a string, turn it into a number by using parseInt
+  products.find((p) => p.id === parseInt(id));
 });
