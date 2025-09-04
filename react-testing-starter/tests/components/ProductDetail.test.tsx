2704939f71d7f9bb7cd3bedd7a27a58c4992bcd4
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-09-04 02:29:44 -0400

    🧼

--- react-testing-starter/tests/components/ProductDetail.test.tsx
+++ react-testing-starter/tests/components/ProductDetail.test.tsx
@@ -8,33 +8,25 @@ import { server } from "../mocks/server";
 describe("ProductDetail", () => {
   let productId: number;
 
-  // 1.) Create a database to render before all our tests
   beforeAll(() => {
-    // 2.) Delcare [product] using the {create} method on db's {product} method
     const product = db.product.create();
-    // 3.) Store the product id created into [productID]
     productId = product.id;
   });
 
-  // 4.) Create a clean up after our tests are completed
   afterAll(() => {
-    // 5.) Use db's {product} method's {delete} method. Give it an object of where, id, equals: (our productId)
     db.product.delete({ where: { id: { equals: productId } } });
   });
 
   it("should render the product details", async () => {
-    // 6.) Use findFirst() to find the product that equals the productId and store it in [product]
     const product = db.product.findFirst({
       where: { id: { equals: productId } },
     });
     render(<ProductDetail productId={productId} />, { wrapper: AllProviders });
 
-    // 7.) Use [product] to verify the name. Use a `!` because we KNOW the product is not null
     expect(
       await screen.findByText(new RegExp(product!.name))
     ).toBeInTheDocument();
     expect(
-      // 8.) Use [product] to verify the price. Use a `!` because we KNOW the product is not null
       await screen.findByText(new RegExp(product!.price.toString()))
     ).toBeInTheDocument();
   });
