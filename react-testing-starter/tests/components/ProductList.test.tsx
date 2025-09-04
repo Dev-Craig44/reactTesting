6858ac3fa17aae0d80e5d84f2c3922702d07c991
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-09-04 02:27:09 -0400

    🧠 test: Write test for errors: ProductList

--- react-testing-starter/tests/components/ProductList.test.tsx
+++ react-testing-starter/tests/components/ProductList.test.tsx
@@ -40,11 +40,15 @@ describe("ProductList", () => {
     expect(message).toBeInTheDocument();
   });
 
+  // 1.) Create test case for when there is no products
   it("should render an error message when there is an error", async () => {
+    // 2.) Simulate a no response by using the {use} on the server object. Instead of {json} we will call {error} on the request handler.
     server.use(http.get("/products", () => HttpResponse.error()));
 
+    // 3.) Render our component
     render(<ProductList />, { wrapper: AllProviders });
 
+    // 4.) Because the message will pop up asynchronuosly we fill findByText and verify it will be in the document
     expect(await screen.findByText(/error/i)).toBeInTheDocument();
   });
 
