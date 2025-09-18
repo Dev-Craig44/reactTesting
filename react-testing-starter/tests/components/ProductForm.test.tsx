dff41642faf2970a0635b6626ae2848eaffcd779
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-09-18 01:03:24 -0400

    🧠 Finished refactorting ProductForm

--- react-testing-starter/tests/components/ProductForm.test.tsx
+++ react-testing-starter/tests/components/ProductForm.test.tsx
@@ -27,28 +27,38 @@ describe("ProductForm", () => {
     return {
       // 8.) declare our waitForFormToLoad function
       waitForFormToLoad: () => screen.findByRole("form"),
-      // 12.) Add the nameInput function
-      // 14.) defer nameInput rendering
-      getNameInput: () => screen.getByPlaceholderText(/name/i),
+      // 15.) Create a function that returns an object with all our inputs
+      getInputs: () => {
+        return {
+          // 12.) Add the nameInput function
+          // 14.) defer nameInput rendering
+          nameInput: screen.getByPlaceholderText(/name/i),
+          // 16.) Add priceInput
+          priceInput: screen.getByPlaceholderText(/price/i),
+          // 17.) Add categoryInput
+          categoryInput: screen.getByRole("combobox", { name: /category/i }),
+        };
+      },
     };
   };
 
   it("should render form fields", async () => {
     // 3.) Call the render function
     // 9.) Destruct the component and grab our wait function
-    const { waitForFormToLoad, getNameInput } = renderComponent();
+    // 18.) Replace w/ getInputs
+    const { waitForFormToLoad, getInputs } = renderComponent();
 
     // 10.) replace our logic with our helper function
     await waitForFormToLoad();
     // 13.) replace old logic with nameInput function
     // Because nameInput is render when the renderComponent is rendered we need to turn it into a function
-    expect(getNameInput()).toBeInTheDocument();
+    // 19.) Call input helper and destructure the inputs we need
+    const { nameInput, priceInput, categoryInput } = getInputs();
 
-    expect(screen.getByPlaceholderText(/price/i)).toBeInTheDocument();
-
-    expect(
-      screen.getByRole("combobox", { name: /category/i })
-    ).toBeInTheDocument();
+    // 20.) Replace all input logic with our helpers
+    expect(nameInput).toBeInTheDocument();
+    expect(priceInput).toBeInTheDocument();
+    expect(categoryInput).toBeInTheDocument();
   });
 
   it("should populate form fields when editing a product", async () => {
@@ -60,21 +70,17 @@ describe("ProductForm", () => {
     };
     // 4.) Replace this with our render function and give it our [product].
     // 11.) Grab hour helper function and replace it with it's old implementation
-    const { waitForFormToLoad } = renderComponent(product);
+    // 21.) grab inputs helper function
+    const { waitForFormToLoad, getInputs } = renderComponent(product);
 
     // 11.)
     await waitForFormToLoad();
 
-    expect(await screen.findByPlaceholderText(/name/i)).toHaveValue(
-      product.name
-    );
+    // 22.) Call inputs helper and destructure our inputs, and replace old logic
+    const { nameInput, priceInput, categoryInput } = getInputs();
 
-    expect(screen.getByPlaceholderText(/price/i)).toHaveValue(
-      product.price.toString()
-    );
-
-    expect(
-      screen.getByRole("combobox", { name: /category/i })
-    ).toHaveTextContent(category.name);
+    expect(nameInput).toHaveValue(product.name);
+    expect(priceInput).toHaveValue(product.price.toString());
+    expect(categoryInput).toHaveTextContent(category.name);
   });
 });
