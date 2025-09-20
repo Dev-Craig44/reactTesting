eb2bf7b8ffd7b0eb8819105a0bc0e2e4a3ba0b1c
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-09-20 01:53:45 -0400

    🧠 Paramerize our test cases for the price validation: ProductForm

--- react-testing-starter/tests/components/ProductForm.test.tsx
+++ react-testing-starter/tests/components/ProductForm.test.tsx
@@ -94,4 +94,58 @@ describe("ProductForm", () => {
       expect(error).toHaveTextContent(errorMessage);
     }
   );
+
+  // 1.) Duplicate previous test
+  it.each([
+    { scenario: "missing", errorMessage: /required/i },
+    {
+      // 2.) Change scenario to less than 1
+      scenario: "0",
+      // 3.) Change [name] to [price]
+      price: "0",
+      errorMessage: /1/,
+    },
+    {
+      // 4.) Change scenario to negative
+      scenario: "negative",
+      // 5.) Change price to -1
+      price: "-1",
+      errorMessage: /1/,
+    },
+    {
+      // 6.) Change scenario to greater than 1000
+      scenario: "not a number",
+      // 7.) Change price to 1001
+      price: "a",
+      errorMessage: /required/,
+    },
+    {
+      // 11.) Change scenario to greater than 1000
+      scenario: "greater than 1000",
+      // 12.) Change price to 1001
+      price: "1001",
+      errorMessage: /1000/,
+    },
+  ])(
+    // 8.) Change [name] to [price]
+    "should display an error if price is $scenario",
+    async ({ price, errorMessage }) => {
+      const { waitForFormToLoad } = renderComponent();
+      const form = await waitForFormToLoad();
+
+      const user = userEvent.setup();
+      // 9.) Ch ange name to a valid name
+      await user.type(form.nameInput, "a");
+      // 10.) Check if price is not undefined then give it our [price] variable
+      if (price !== undefined) await user.type(form.priceInput, price);
+      await user.click(form.categoryInput);
+      const options = screen.getAllByRole("option");
+      await user.click(options[0]);
+      await user.click(form.submitButton);
+
+      const error = screen.getByRole("alert");
+      expect(error).toBeInTheDocument();
+      expect(error).toHaveTextContent(errorMessage);
+    }
+  );
 });
